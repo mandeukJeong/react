03d91e3e598357ccbf354513c2563d38b8ff6eb0
mandeukJeong <wjdalstj137@gmail.com>
2024-02-20 18:16:52 +0900

    part1 - useState 과제

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [title, b] = useState('남자 코트 추천');
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
 
   return (
     <div className="App">
@@ -10,7 +10,15 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{title}</h4>
+        <h4>{title[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>

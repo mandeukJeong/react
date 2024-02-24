44ab9c021fb7e4a8a47db32bc138f8dfb5980f2e
mandeukJeong <wjdalstj137@gmail.com>
2024-02-24 18:48:03 +0900

    part2 - 이미지 넣기, public image

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,19 +1,54 @@
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+// import bg from './img/bg.png';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      {/* <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div> */}
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src={process.env.PUBLIC_URL + '/logo192.png'}
+              alt="신발1"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              alt="신발2"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              alt="신발3"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

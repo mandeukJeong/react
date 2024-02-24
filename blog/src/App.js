544bc365c3400f6ed8652b5834df50cb55ec251e
mandeukJeong <wjdalstj137@gmail.com>
2024-02-24 17:02:52 +0900

    part1 - map

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
-  let [good, setGood] = useState(0);
+  let [title, setTitle] = useState([
+    '남자 코트 추천',
+    '강남 우동 맛집',
+    '파이썬독학',
+  ]);
   let [modal, setModal] = useState(false);
 
   return (
@@ -11,32 +14,22 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button onClick={() => {
-        let copy = [...title];
-        copy.sort();
-        setTitle(copy);
-      }}>가나다순정렬</button>
-      <button onClick={() => {
-        let copy = [...title];
-        copy[0] = '여자 코트 추천';
-        setTitle(copy);
-      }}>글수정</button>
-      <div className="list">
-        <h4>{title[0]} <span onClick={() => {setGood(good + 1);}}>👍</span> {good} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={() => {
-          setModal((prevState) => {
-            return !prevState;
-          });
-        }}>{title[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {title.map((item) => {
+        return (
+          <div className="list">
+            <h4
+              onClick={() => {
+                setModal((prevState) => {
+                  return !prevState;
+                });
+              }}
+            >
+              {item}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
       {modal === true ? <Modal /> : null}
     </div>
   );
@@ -44,11 +37,11 @@ function App() {
 
 function Modal() {
   return (
-    <div className='modal'>
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   );
 }
 

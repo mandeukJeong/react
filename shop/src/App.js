700c1fe635295d9545baba3f1eb48659dfd7cf6a
mandeukJeong <wjdalstj137@gmail.com>
2024-02-25 23:00:43 +0900

    part2 - import, export

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import data from './data';
 // import bg from './img/bg.png';
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" data-bs-theme="light">
@@ -22,12 +26,12 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img
-              src={process.env.PUBLIC_URL + '/logo192.png'}
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
               alt="신발1"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img
@@ -35,8 +39,8 @@ function App() {
               alt="신발2"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img
@@ -44,8 +48,8 @@ function App() {
               alt="신발3"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
